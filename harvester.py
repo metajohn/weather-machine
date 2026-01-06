56233406edcd488eda6c46d20993ec43cc0590b9
metajohn <jvanen@gmail.com>
2026-01-05 22:32:19 -0800

    feat: implement automated weather harvester to db pipeline

--- harvester.py
+++ harvester.py
@@ -2,7 +2,21 @@ import os
 import requests
 from dotenv import load_dotenv
 import sqlite3
+import time
 
+#----------SETUP---------
+#load the .env as an environment variable
+load_dotenv()
+API_KEY = os.getenv("OPENWEATHER_API_KEY")
+#This is a test location, currently the space needle
+LAT = 47.62050853108323
+LON = -122.34928075550654
+#Create the url from the relevant parts, currently latitude, longitude, and the api key
+URL = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric"
+
+SLEEPTIME_SECONDS = 600
+
+#---------DEFINITION----------
 def init_db():
 #    """Creates db file and table if they don't exist"""
     conn = sqlite3.connect("weather_data.db")
@@ -22,50 +36,58 @@ def init_db():
     conn.commit()
     #close db
     conn.close()
-
     print("Database check: Table is ready.")
 
-if __name__ == "__main__":
+
+def harvest_once():
+    response = requests.get(URL, timeout=10)
+    if response.status_code == 200:
+        data = response.json()
+
+        #Extraction
+        temp = data['main']['temp']
+        humid = data ['main']['humidity']
+        wind = data['wind']['speed']
+        desc = data['weather'][0]['description']
+
+        #Connection
+        conn = sqlite3.connect("weather_data.db")
+        cursor = conn.cursor()
+
+        #Execution
+        cursor.execute('''
+            INSERT INTO seattle_weather (temp_c, humidity_pct, wind_speed_ms, description)
+            VALUES (?, ?, ?, ?)
+        ''', (temp, humid, wind, desc))
+
+        conn.commit()
+        conn.close()
+        print("Weather recorded in Database")
+        print(f"Weather:\nTemperature: {temp}°C\nHumidity: {humid}\nWind Speed: {wind}\nDescription: {desc}")
+    else:
+        print("API Error: Weather NOT recorded")
+        print(f"Status: {response.status_code}")
+        print(f"Message: {response.text}")
+
+def main():
     init_db()
 
-#load the .env as an environment variable
-load_dotenv()
+    while True:
+        try: 
+            harvest_once()
+        except Exception as e:
+            print(f"Something went wrong: {e}")
+        
+        #sanity check to prevent API calls under a minute
+        actual_sleep = max(60, SLEEPTIME_SECONDS)
 
-API_KEY = os.getenv("OPENWEATHER_API_KEY")
-
-#This is a test location, currently the space needle
-LAT = 47.62050853108323
-LON = -122.34928075550654
-
-#Create the url from the relevant parts, currently latitude, longitude, and the api key
-url = f"https://api.openweathermap.org/data/2.5/weather?lat={LAT}&lon={LON}&appid={API_KEY}&units=metric"
+        #display the sleeptime in minutes
+        display_sleeptime_minutes = actual_sleep / 60
+        print(f"sleeping for {display_sleeptime_minutes:.0f} mintues...")
+        time.sleep(actual_sleep)
+        
 
 
-response = requests.get(url)
-
-if response.status_code == 200:
-    data = response.json()
-
-    #Extraction
-    temp = data['main']['temp']
-    humid = data ['main']['humidity']
-    wind = data['wind']['speed']
-    desc = data['weather'][0]['description']
-
-    #Connection
-    conn = sqlite3.connect("weather_data.db")
-    cursor = conn.cursor()
-
-    #Execution
-    cursor.execute('''
-        INSERT INTO seattle_weather (temp_c, humidity_pct, wind_speed_ms, description)
-        VALUES (?, ?, ?, ?)
-    ''', (temp, humid, wind, desc))
-
-    conn.commit()
-    conn.close()
-    print("Weather Recorded in Database")
-    print(f"Weather:\nTemperature: {temp}°C\nHumidity: {humid}\nWind Speed: {wind}\nDescription: {desc}")
-else:
-    print(f"Status: {response.status_code}")
-    print(f"Message: {response.text}")
+#---------EXECUTION---------
+if __name__ == "__main__":
+    main()
